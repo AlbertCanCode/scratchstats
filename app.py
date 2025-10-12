1d51cd5228b6373693225503ff13a8c8643b663b
AlbertCanCode <as.gamecoder@gmail.com>
2025-10-12 18:52:47 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template_string, request
 import requests
 
 app = Flask(__name__)
 
+# Read HTML from the file directly
+with open("index.html", "r") as f:
+    HTML = f.read()
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template_string(HTML)
 
 @app.route("/stats", methods=["POST"])
 def stats():
